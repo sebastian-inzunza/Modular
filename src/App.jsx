fb0a5fd9cdbdc9b1582010cd5faf6642624fb9a9
Sebqastian <sebastian_a9@hotmail.com>
2023-05-24 21:09:19 -0600

    First commint

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import Content from "./components/Content"
+
+
+function App() {
+
+  return (
+    <>
+    <Header />
+    <Content />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Content.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ImagenBody from "../asset/3.jpg"
+
+function Body() {
+  return (
+    <div className='bg-slate-900'>
+      werwer
+    </div>
+  )
+}
+
+export default Body
